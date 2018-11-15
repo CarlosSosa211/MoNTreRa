cdf860264dfcea257902b977455d8d180bb2219e
CarlosSosa211 <avefenix.211@gmail.com>
2018-11-15 12:36:45 +0100

    toyModel case added

--- Matlab/morrisVarRange.m
+++ Matlab/morrisVarRange.m
@@ -15,26 +15,26 @@ global b color nfig shape;
 %     '$V_{max}^{O_2}$', '$K_M^{O_2}$', '$D^{VEGF}$', '$V_{max}^{VEGF}$'...
 %     '$K_M^{VEGF}$', '$pO_2^{preEnd}$', '$pO_2^{neoEnd}$', '$pO_2^{hyp}$', '$v^{hyp}$'};
 
-nfig = 1;
+nfig = 0;
 quit = 0;
 
 % path = uigetdir('../../Carlos/Results');
 path = '../../Carlos/Results/MorrisVarToyModel_Par2/';
 
-varRange = load([path, 'varRange.dat']);
+varRange = load([path, 'morrisVarRange.res']);
 par = varRange(1) + 1;
 varRange = varRange(2 : length(varRange));
 
 nPar = 5;
-b = {'$x_{1}$', '$x_{2}$', '$x_{3}$', '$x_{4}$', '$x_{5}$'};
+b = {'$x_{0}$', '$x_{1}$', '$x_{2}$', '$x_{3}$', '$x_{4}$'};
 color = linspace(0, 1, length(varRange));
 shape = ['o', 's', 'v', 'd'];
 
 while(~quit)
     output = input('Select an output [timeTo95 (1), timeTo99 (2), tumDens (3) or intTumDens (4)] or quit (0): ');
+    sel = 0;
     switch output
         case 1
-            sel = 1;
             if(sel == 0)
                 for i = 1 : length(varRange)
                     plotTimeTo95(path, varRange(i))
@@ -44,7 +44,6 @@ while(~quit)
             end
             
         case 2
-            sel = 1;
             if(sel == 0)
                 for i = 1 : length(varRange)
                     plotTimeTo99(path, varRange(i))
@@ -54,7 +53,6 @@ while(~quit)
             end
             
         case 3
-            sel = 1;
             if(sel == 0)
                 for i = 1 : length(varRange)
                     plotTumDens(path, varRange(i))
@@ -64,7 +62,6 @@ while(~quit)
             end
             
         case 4
-            sel = 1;
             if(sel == 0)
                 for i = 1 : length(varRange)
                     plotIntTumDens(path, varRange(i))
@@ -72,7 +69,16 @@ while(~quit)
             elseif(sel == 1)
                 plotParIntTumDens(path, par, 6)
             end
-                     
+            
+        case -1
+            if(sel == 0)
+                for i = 1 : length(varRange)
+                    plotY(path, i - 1)
+                end
+            elseif(sel == 1)
+                plotParY(path, par)
+            end
+            
         case 0
             quit = 1;
     end
