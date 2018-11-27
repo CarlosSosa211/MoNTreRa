80a2db095fcc82c5138aaeddc25da331275631ff
CarlosSosa211 <avefenix.211@gmail.com>
2018-11-27 11:03:03 +0100

    adapted to plot the studied parameter or all of them

--- Matlab/morrisVarRange.m
+++ Matlab/morrisVarRange.m
@@ -4,78 +4,88 @@ global nPar
 global varRange;
 global b color nfig shape;
 
-% nPar = 34;
-% b = {'$T_{tum}$', '$T_{heal}$', '$T_{end}$', '$\bar{v}$', '$\alpha_{heal}$'...
-%     '$\alpha/\beta_{heal}$', '$\alpha_{tumG1}$', '$\alpha/\beta_{tumG1}$'...
-%     '$\alpha_{tumS}$', '$\alpha/\beta_{tumS}$' '$\alpha_{tumG2}$'...
-%     '$\alpha/\beta_{tumG2}$', '$\alpha_{tumM}$', '$\alpha/\beta_{tumM}$'...
-%     '$\alpha_{tumG0}$', '$\alpha/\beta_{tumG0}$', '$\alpha_{preEnd}$'...
-%     '$\alpha/\beta_{preEnd}$', '$\alpha_{neoEnd}$', '$\alpha/\beta_{neoEnd}$'...
-%     '$d_{thres}$', '$T_{arrest}$', '$pO_2^{nec}$', '$d$', '$D^{O_2}$'...
-%     '$V_{max}^{O_2}$', '$K_M^{O_2}$', '$D^{VEGF}$', '$V_{max}^{VEGF}$'...
-%     '$K_M^{VEGF}$', '$pO_2^{preEnd}$', '$pO_2^{neoEnd}$', '$pO_2^{hyp}$', '$v^{hyp}$'};
+nPar = 34;
+b = {'$T_{tum}$', '$T_{heal}$', '$T_{end}$', '$\bar{v}$', '$\alpha_{heal}$'...
+    '$\alpha/\beta_{heal}$', '$\alpha_{tumG1}$', '$\alpha/\beta_{tumG1}$'...
+    '$\alpha_{tumS}$', '$\alpha/\beta_{tumS}$' '$\alpha_{tumG2}$'...
+    '$\alpha/\beta_{tumG2}$', '$\alpha_{tumM}$', '$\alpha/\beta_{tumM}$'...
+    '$\alpha_{tumG0}$', '$\alpha/\beta_{tumG0}$', '$\alpha_{preEnd}$'...
+    '$\alpha/\beta_{preEnd}$', '$\alpha_{neoEnd}$', '$\alpha/\beta_{neoEnd}$'...
+    '$d_{thres}$', '$T_{arrest}$', '$pO_2^{nec}$', '$d$', '$D^{O_2}$'...
+    '$V_{max}^{O_2}$', '$K_M^{O_2}$', '$D^{VEGF}$', '$V_{max}^{VEGF}$'...
+    '$K_M^{VEGF}$', '$pO_2^{preEnd}$', '$pO_2^{neoEnd}$', '$pO_2^{hyp}$', '$v^{hyp}$'};
 
 nfig = 0;
 quit = 0;
 
 % path = uigetdir('../../Carlos/Results');
-path = '../../Carlos/Results/MorrisVarToyModel_Par2/';
+% path = '../../Carlos/Results/MorrisVarToyModel_Par2/';
+path = '../../Carlos/Results/MorrisVarRangeCluster/';
 
 varRange = load([path, 'morrisVarRange.res']);
 par = varRange(1) + 1;
 varRange = varRange(2 : length(varRange));
 
-nPar = 5;
-b = {'$x_{0}$', '$x_{1}$', '$x_{2}$', '$x_{3}$', '$x_{4}$'};
-color = linspace(0, 1, length(varRange));
+% nPar = 5;
+% b = {'$x_{0}$', '$x_{1}$', '$x_{2}$', '$x_{3}$', '$x_{4}$'};
 shape = ['o', 's', 'v', 'd'];
 
 while(~quit)
     output = input('Select an output [timeTo95 (1), timeTo99 (2), tumDens (3) or intTumDens (4)] or quit (0): ');
-    sel = 0;
+    sel = input('Select the studied parameter (1) or all of them (2) or: ');
     switch output
         case 1
-            if(sel == 0)
+            if(sel == 2)
                 for i = 1 : length(varRange)
+                    color = linspace(0, 1, nPar);
                     plotTimeTo95(path, varRange(i))
                 end
             elseif(sel == 1)
+                color = linspace(0, 1, length(varRange));
                 plotParTimeTo95(path, par, 6)
             end
             
         case 2
-            if(sel == 0)
+            if(sel == 2)
                 for i = 1 : length(varRange)
+                    color = linspace(0, 1, nPar);
                     plotTimeTo99(path, varRange(i))
                 end
             elseif(sel == 1)
+                color = linspace(0, 1, length(varRange));
                 plotParTimeTo99(path, par, 6)
             end
             
         case 3
-            if(sel == 0)
+            if(sel == 2)
                 for i = 1 : length(varRange)
+                    color = linspace(0, 1, nPar);
                     plotTumDens(path, varRange(i))
                 end
             elseif(sel == 1)
+                color = linspace(0, 1, length(varRange));
                 plotParTumDens(path, par, 6)
             end
             
         case 4
-            if(sel == 0)
+            if(sel == 2)
                 for i = 1 : length(varRange)
+                    color = linspace(0, 1, nPar);
                     plotIntTumDens(path, varRange(i))
                 end
             elseif(sel == 1)
+                color = linspace(0, 1, length(varRange));
                 plotParIntTumDens(path, par, 6)
             end
             
         case -1
-            if(sel == 0)
+            if(sel == 2)
                 for i = 1 : length(varRange)
+                    color = linspace(0, 1, nPar);
                     plotY(path, i - 1)
                 end
             elseif(sel == 1)
+                color = linspace(0, 1, length(varRange));
                 plotParY(path, par)
             end
             
