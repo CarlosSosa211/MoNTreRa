a3c9a03d5e02777f40bab5211d22bf3b6ed6f349
CarlosSosa211 <avefenix.211@gmail.com>
2018-12-13 10:28:17 +0100

    Some graphics added

--- Matlab/errors.m
+++ Matlab/errors.m
@@ -1,10 +1,10 @@
 clear all
 close all
 
-global nfig
 nfig = 0;
 
-path = '../../Carlos/Results/Err_Ang_250Sim';
+% path = '../../Carlos/Results/Err_Ang_250Sim';
+path = '../../Carlos/Results/Err_AlphaBeta_432Sim';
 
 nOut = 8;
 errEndTreatTumDens = load([path, '/errEndTreatTumDens.res']);
@@ -167,12 +167,31 @@ xlabel('Dose (Gy)')
 ylabel('Error')
 
 %%
+errRecTimeMeanTTum = [];
+errRecTimeStdTTum = [];
+for i = 1:length(tTTum)
+    errRecTimeMeanTTum = [errRecTimeMeanTTum...
+        mean(errRecTime(errRecTime(:, 3) == tTTum(i), 2))];
+    errRecTimeStdTTum = [errRecTimeStdTTum...
+        mean(errRecTime(errRecTime(:, 3) == tTTum(i), 2))];
+end
+
+nfig = nfig + 1;
+figure(nfig)
+errorbar(tTTum, errRecTimeMeanTTum, errRecTimeStdTTum,...
+    '-s', 'MarkerSize', 10, 'MarkerEdgeColor', 'k', 'MarkerFaceColor', 'k')
+title('Rel. error in time to recurrence')
+grid on
+xlabel('TTum (h)')
+ylabel('Error')
+
+%%
 errEndTreatTumDensTTumDose = zeros(length(tTTum), length(tDose));
 for i = 1:length(tTTum)
     for j = 1:length(tDose)
         errEndTreatTumDensTTumDose(i, j) =...
-            mean(errEndTreatTumDens(err3MonTumDens(:, 3) == tTTum(i) &...
-            err3MonTumDens(:, 5) == tDose(j), 1));
+            mean(errEndTreatTumDens(errEndTreatTumDens(:, 3) == tTTum(i) &...
+            errEndTreatTumDens(:, 5) == tDose(j), 1));
     end
 end
 
@@ -181,20 +200,20 @@ figure(nfig)
 image(errEndTreatTumDensTTumDose, 'CDataMapping','scaled')
 colorbar
 title('Abs. error in tumor density at the end of treat.')
-xlabel('TTum (h)')
-ylabel('Dose (Gy)')
-xticks(1:length(tTTum))
-xticklabels(tTTum)
-yticks(1:length(tDose))
-yticklabels(tDose)
+xlabel('Dose (Gy)')
+ylabel('TTum (h)')
+xticks(1:length(tDose))
+xticklabels(tDose)
+yticks(1:length(tTTum))
+yticklabels(tTTum)
 
 %%
 errEndTreatTumDensDThresDose = zeros(length(tDThres), length(tDose));
 for i = 1:length(tDThres)
     for j = 1:length(tDose)
         errEndTreatTumDensDThresDose(i, j) =...
-            mean(errEndTreatTumDens(err3MonTumDens(:, 4) == tDThres(i) &...
-            err3MonTumDens(:, 5) == tDose(j), 1));
+            mean(errEndTreatTumDens(errEndTreatTumDens(:, 4) == tDThres(i) &...
+            errEndTreatTumDens(:, 5) == tDose(j), 1));
     end
 end
 
@@ -203,20 +222,20 @@ figure(nfig)
 image(errEndTreatTumDensDThresDose, 'CDataMapping','scaled')
 colorbar
 title('Abs. error in tumor density at the end of treat.')
-xlabel('Dthres (Gy)')
-ylabel('Dose (Gy)')
-xticks(1:length(tDThres))
-xticklabels(tDThres)
-yticks(1:length(tDose))
-yticklabels(tDose)
+xlabel('Dose (Gy)')
+ylabel('Dthres (Gy)')
+xticks(1:length(tDose))
+xticklabels(tDose)
+yticks(1:length(tDThres))
+yticklabels(tDThres)
 
 %%
 errEndTreatTumDensTTumDThres = zeros(length(tTTum), length(tDThres));
 for i = 1:length(tTTum)
     for j = 1:length(tDThres)
         errEndTreatTumDensTTumDThres(i, j) =...
-            mean(errEndTreatTumDens(err3MonTumDens(:, 3) == tTTum(i) &...
-            err3MonTumDens(:, 4) == tDThres(j), 1));
+            mean(errEndTreatTumDens(errEndTreatTumDens(:, 3) == tTTum(i) &...
+            errEndTreatTumDens(:, 4) == tDThres(j), 1));
     end
 end
 
@@ -225,20 +244,20 @@ figure(nfig)
 image(errEndTreatTumDensTTumDThres, 'CDataMapping','scaled')
 colorbar
 title('Abs. error in tumor density at the end of treat.')
-xlabel('TTum (h)')
-ylabel('DThres (Gy)')
-xticks(1:length(tTTum))
-xticklabels(tTTum)
-yticks(1:length(tDThres))
-yticklabels(tDThres)
+xlabel('DThres (Gy)')
+ylabel('TTum (h)')
+xticks(1:length(tDThres))
+xticklabels(tDThres)
+yticks(1:length(tTTum))
+yticklabels(tTTum)
 
 %%
 errIntTumDensDThresDose = zeros(length(tDThres), length(tDose));
 for i = 1:length(tDThres)
     for j = 1:length(tDose)
         errIntTumDensDThresDose(i, j) =...
-            mean(errIntTumDens(err3MonTumDens(:, 4) == tDThres(i) &...
-            err3MonTumDens(:, 5) == tDose(j), 1));
+            mean(errIntTumDens(errIntTumDens(:, 4) == tDThres(i) &...
+            errIntTumDens(:, 5) == tDose(j), 2));
     end
 end
 
@@ -246,10 +265,76 @@ nfig = nfig + 1;
 figure(nfig)
 image(errIntTumDensDThresDose, 'CDataMapping','scaled')
 colorbar
-title('Abs. error in integral of tumor density')
-xlabel('Dthres (Gy)')
-ylabel('Dose (Gy)')
-xticks(1:length(tDThres))
-xticklabels(tDThres)
-yticks(1:length(tDose))
-yticklabels(tDose)
+title('Rel. error in integral of tumor density')
+xlabel('Dose (Gy)')
+ylabel('Dthres (Gy)')
+xticks(1:length(tDose))
+xticklabels(tDose)
+yticks(1:length(tDThres))
+yticklabels(tDThres)
+
+%%
+errIntTumDensTTumDose = zeros(length(tTTum), length(tDose));
+for i = 1:length(tTTum)
+    for j = 1:length(tDose)
+        errIntTumDensTTumDose(i, j) =...
+            mean(errIntTumDens(errIntTumDens(:, 3) == tTTum(i) &...
+            errIntTumDens(:, 5) == tDose(j), 2));
+    end
+end
+
+nfig = nfig + 1;
+figure(nfig)
+image(errIntTumDensTTumDose, 'CDataMapping','scaled')
+colorbar
+title('Rel. error in integral of tumor density')
+xlabel('Dose (Gy)')
+ylabel('TTum (h)')
+xticks(1:length(tDose))
+xticklabels(tDose)
+yticks(1:length(tTTum))
+yticklabels(tTTum)
+
+%%
+errTimeTo95TTumDens = zeros(length(tTTum), length(tDose));
+for i = 1:length(tTTum)
+    for j = 1:length(tDose)
+        errTimeTo95TTumDose(i, j) =...
+            mean(errTimeTo95(errTimeTo95(:, 3) == tTTum(i) &...
+            errTimeTo95(:, 5) == tDose(j), 2));
+    end
+end
+
+nfig = nfig + 1;
+figure(nfig)
+image(errTimeTo95TTumDose, 'CDataMapping','scaled')
+colorbar
+title('Rel. error in time to kill 95% of tumor cells')
+xlabel('Dose (Gy)')
+ylabel('TTum (h)')
+xticks(1:length(tDose))
+xticklabels(tDose)
+yticks(1:length(tTTum))
+yticklabels(tTTum)
+
+%%
+errRecTimeTTumDens = zeros(length(tTTum), length(tDose));
+for i = 1:length(tTTum)
+    for j = 1:length(tDose)
+        errRecTimeTTumDose(i, j) =...
+            mean(errRecTime(errRecTime(:, 3) == tTTum(i) &...
+            errRecTime(:, 5) == tDose(j), 2));
+    end
+end
+
+nfig = nfig + 1;
+figure(nfig)
+image(errRecTimeTTumDose, 'CDataMapping','scaled')
+colorbar
+title('Rel. error in time to recurrence')
+xlabel('Dose (Gy)')
+ylabel('TTum (h)')
+xticks(1:length(tDose))
+xticklabels(tDose)
+yticks(1:length(tTTum))
+yticklabels(tTTum)
