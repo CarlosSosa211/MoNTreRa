a7883a20d85398bcc1c10fb659ec67ba7b3df85a
CarlosSosa211 <avefenix.211@gmail.com>
2019-01-15 16:41:31 +0100

    First version

---
+++ buildAndRunAllTissues.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+rm -rf OutputFiles/
+rm -rf build/
+mkdir build
+cmake -Bbuild -Hsrc
+cd build
+make
+for i in `seq 1 21`;
+do
+mkdir ../OutputFiles
+cp ../HistSpec/tissueDim$i.dat ../InputFiles/tissueDim.dat
+cp ../HistSpec/inTum$i.dat ../InputFiles/inTum.dat
+cp ../HistSpec/inVes$i.dat ../InputFiles/inVes.dat
+./RTsim
+mv ../OutputFiles ../OutputFiles$i
+done
+
