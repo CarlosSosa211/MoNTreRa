1cf5e5876e34210622f9086d7401daaa79a75ac1
CarlosSosa211 <avefenix.211@gmail.com>
2019-04-18 17:53:01 +0200

    Blocks are now generic and tissues can be sorted according to their initial vascular density

--- Matlab/diffScalOutputs.m
+++ Matlab/diffScalOutputs.m
@@ -14,33 +14,38 @@ close all
 nfig = 0;
 % path = '../../Carlos/Results/Diff/Ang_Dose_5Val_5Rep/Tissue';
 % path = '../../Carlos/Results/Diff/Res_Dose_5Val_5Rep/Tissue';
-% path = '../../Carlos/Results/Diff/AngRes_Dose_5Val_5Rep/Tissue';
-% path = '../../Carlos/Results/Diff/HypNec_Dose_5Val_5Rep/Tissue';
-% path = '../../Carlos/Results/Diff/Ang_10x3Sim_noHypNec/Tissue';
 % path = '../../Carlos/Results/Diff/Arrest_Dose_5Val_5Rep/Tissue';
+path = '../../Carlos/Results/Diff/HypNec_Dose_5Val_5Rep/Tissue';
 % path = '../../Carlos/Results/Diff/Oxy_Dose_5Val_5Rep/Tissue';
+% path = '../../Carlos/Results/Diff/AngRes_Dose_5Val_5Rep/Tissue';
+% path = '../../Carlos/Results/Diff/ArrestNoAngNoRes_Dose_5Val_5Rep/Tissue';
 % path = '../../Carlos/Results/Diff/OxyNoHypNec_Dose_5Val_5Rep/Tissue';
-path = '../../Carlos/Results/Diff/OxyNoAngNoHypNec_Dose_5Val_5Rep/Tissue';
+% path = '../../Carlos/Results/Diff/OxyNoAngNoHypNec_Dose_5Val_5Rep/Tissue';
+
+initVascDens = load('../HistSpec/initVascDens.dat');
 
 nTissues = 21;
+[initVascDens, tissuesVascDens] = sort(initVascDens);
 nOut = 15;
 
 % withoutN = 'No angiogenesis';
 % withN = 'Angiogenesis';
 % withoutN = 'No healthy cell division';
 % withN = 'Healthy cell division';
-% withoutN = 'No angiogenesis and no healthy cell division';
-% withN = 'Angiogenesis and healthy cell division';
-% withoutN = 'No hypoxic necrosis';
-% withN = 'Hypoxic necrosis';
 % withoutN = 'No arrest';
 % withN = 'Arrest';
+withoutN = 'No hypoxic necrosis';
+withN = 'Hypoxic necrosis';
 % withoutN = 'No oxyegenation (no hypoxic necrosis)';
 % withN = 'Oxygenation';
+% withoutN = 'No angiogenesis and no healthy cell division';
+% withN = 'Angiogenesis and healthy cell division';
+% withoutN = 'No arrest, no angiogenesis, no healthy cell division';
+% withN = 'Arrest, no angiogenesis, no healthy cell division';
 % withoutN = 'No oxyegenation (no hypoxic necrosis)';
 % withN = 'Oxygenation (no hypoxic necrosis)';
-withoutN = 'No oxyegenation (no hypoxic necrosis)';
-withN = 'Oxygenation (no angiogenesis, no hypoxic necrosis)';
+% withoutN = 'No oxyegenation (no hypoxic necrosis)';
+% withN = 'Oxygenation (no angiogenesis, no hypoxic necrosis)';
 
 fileNames = {'/endTreatTumDens.res', '/3MonTumDens.res'...
     '/finTumVol.res', '/intTumDens.res', '/killed50.res'...
@@ -70,13 +75,13 @@ outputNames = {'tumour density at the end of treat.'...
 % corresponds to a combination of parameters. The first column corresponds
 % to the mean values for nRep repetitions of X_0_X simulations; the second
 % one, to the mean values of X_1_X simulations; the third one, two the std
-% values of X_0_X simulations and the fourth one, to the std values of 
+% values of X_0_X simulations and the fourth one, to the std values of
 % X_1_X simulations. For the single tissue - multiple outputs case
 % (1 <= nTissue <= nTissues and selOut = 0), each layer correponds to an
 % output. For the multiple tissues - single output case, (nTissue = 0 and
 % 1 <= selOut <= nOut) each layer corresponds to a tissue. Finally, for the
 % mean over the tissues - single output case (nTissue = -1 and
-% 1 <= selOut <= nOut), the matrix has a single layer with the mean values.  
+% 1 <= selOut <= nOut), the matrix has a single layer with the mean values.
 
 nTissue = input(['Select one tissue (from 1 to ', num2str(nTissues)...
     ') or all of them (0) or a mean over them (-1): ']);
@@ -157,31 +162,49 @@ outputDiff = output(:, 1)' - output(:, 2)';
 outputAbsDiff = abs(outputDiff);
 
 %%
-% This block plots the mean and std of the selected output for simulations
-% X_0_X and X_1_1 as a function of the studied TTum values
+selPar = 4;
+switch selPar
+    case 1
+        parName = 'TTum (h)';
+        parT = tTum;
+        parCol = colTTum;
+    case 2
+        parName = 'Dose threshold (Gy)';
+        parT = tDose;
+        parCol = colDose;
+    case 3
+        parName = 'TArrest (h)';
+        parT = tTArrest;
+        parCol = colTArrest;
+    case 4
+        parName = 'Dose (Gy)';
+        parT = tDose;
+        parCol = colDose;        
+end
 
-withoutMeanTTum = [];
-withoutStdTTum = [];
-withMeanTTum = [];
-withStdTTum = [];
-for i = 1:length(tTTum)
-    withoutMeanTTum = [withoutMeanTTum...
-        mean(output(par(:, colTTum) == tTTum(i), 1))];
-    withoutStdTTum = [withoutStdTTum...
-        mean(output(par(:, colTTum) == tTTum(i), 3))];
-    withMeanTTum = [withMeanTTum...
-        mean(output(par(:, colTTum) == tTTum(i), 2))];
-    withStdTTum = [withStdTTum...
-        mean(output(par(:, colTTum) == tTTum(i), 4))];
+%%
+% This block plots the mean and std of the selected output for simulations
+% X_0_X and X_1_1 as a function of the studied values of the parameter par
+
+withoutMean = zeros(1, length(parT));
+withoutStd = zeros(1, length(parT));
+withMean = zeros(1, length(parT));
+withStd = zeros(1, length(parT));
+
+for i = 1:length(parT)
+    withoutMean(i) = mean(output(par(:, parCol) == parT(i), 1));
+    withoutStd(i) = mean(output(par(:, parCol) == parT(i), 3));
+    withMean(i) = mean(output(par(:, parCol) == parT(i), 2));
+    withStd(i) = mean(output(par(:, parCol) == parT(i), 4));
 end
 
 nfig = nfig + 1;
 figure(nfig)
 hold on
-errorbar(tTTum, withoutMeanTTum, withoutStdTTum,...
-    '-s', 'MarkerSize', 10, 'MarkerEdgeColor', 'k', 'MarkerFaceColor', 'k')
-errorbar(tTTum, withMeanTTum, withStdTTum,...
-    '-s', 'MarkerSize', 10, 'MarkerEdgeColor', 'k', 'MarkerFaceColor', 'k')
+errorbar(parT, withoutMean, withoutStd, 'sb', 'MarkerSize', 10,...
+    'MarkerEdgeColor', 'b', 'MarkerFaceColor', 'b')
+errorbar(parT, withMean, withStd, 'sr', 'MarkerSize', 10,...
+    'MarkerEdgeColor', 'r', 'MarkerFaceColor', 'r')
 hold off
 if(nTissue == - 1)
     title(['All tissues - ', char(outputNames(selOut))])
@@ -189,190 +212,28 @@ else
     title(['Tissue ', num2str(nTissue), ' - '...
         char(outputNames(selOut))])
 end
-legend(withoutN, withN, 'location', 'northwest')
-grid on
-xlabel('TTum (h)')
-ylabel(outputNames(selOut))
-
-%%
-% This block plots the mean and std of the abs. difference of the selected 
-% output for simulations X_0_X and X_1_1 as a function of the studied TTum
-% values
-
-diffMeanTTum = [];
-diffStdTTum = [];
-for i = 1:length(tTTum)
-    diffMeanTTum = [diffMeanTTum...
-        mean(outputAbsDiff(par(:, colTTum) == tTTum(i)))];
-    diffStdTTum = [diffStdTTum...
-        std(outputAbsDiff(par(:, colTTum) == tTTum(i)))];
-end
-
-nfig = nfig + 1;
-figure(nfig)
-errorbar(tTTum, diffMeanTTum, diffStdTTum,...
-    '-s', 'MarkerSize', 10, 'MarkerEdgeColor', 'k', 'MarkerFaceColor', 'k')
-if(nTissue == -1)
-    title(['All tissues - Abs. diff. in ', char(outputNames(selOut))])
-else
-    title(['Tissue ', num2str(nTissue), ' - Abs. diff. in '...
-        char(outputNames(selOut))])
-end
-grid on
-xlabel('TTum (h)')
-ylabel('Difference')
-
-%%
-% This block plots the mean and std of the selected output for simulations
-% X_0_X and X_1_1 as a function of the studied DThres values
-
-withoutMeanDThres = [];
-withoutStdDThres = [];
-withMeanDThres = [];
-withStdDThres = [];
-for i = 1:length(tDThres)
-    withoutMeanDThres = [withoutMeanDThres...
-        mean(output(par(:, colDThres) == tDThres(i), 1))];
-    withoutStdDThres = [withoutStdDThres...
-        mean(output(par(:, colDThres) == tDThres(i), 3))];
-    withMeanDThres = [withMeanDThres...
-        mean(output(par(:, colDThres) == tDThres(i), 2))];
-    withStdDThres = [withStdDThres...
-        mean(output(par(:, colDThres) == tDThres(i), 4))];
-end
-
-nfig = nfig + 1;
-figure(nfig)
-hold on
-errorbar(tDThres, withoutMeanDThres, withoutStdDThres,...
-    '-s', 'MarkerSize', 10, 'MarkerEdgeColor', 'k', 'MarkerFaceColor', 'k')
-errorbar(tDThres, withMeanDThres, withStdDThres,...
-    '-s', 'MarkerSize', 10, 'MarkerEdgeColor', 'k', 'MarkerFaceColor', 'k')
-hold off
-if(nTissue == -1)
-    title(['All tissues - ', char(outputNames(selOut))])
-else
-    title(['Tissue ', num2str(nTissue), ' - '...
-        char(outputNames(selOut))])
-end
-legend(withoutN, withN, 'location', 'northwest')
-grid on
-xlabel('Dose threshold (Gy)')
-ylabel(outputNames(selOut))
-
-%%
-% This block plots the mean and std of the abs. difference of the selected 
-% output for simulations X_0_X and X_1_1 as a function of the studied
-% Dthres values
-
-diffMeanDThres = [];
-diffStdDThres = [];
-for i = 1:length(tDThres)
-    diffMeanDThres = [diffMeanDThres...
-        mean(outputAbsDiff(par(:, colDThres) == tDThres(i)))];
-    diffStdDThres = [diffStdDThres...
-        std(outputAbsDiff(par(:, colDThres) == tDThres(i)))];
-end
-
-nfig = nfig + 1;
-figure(nfig)
-errorbar(tDThres, diffMeanDThres, diffStdDThres,...
-    '-s', 'MarkerSize', 10, 'MarkerEdgeColor', 'k', 'MarkerFaceColor', 'k')
-if(nTissue == -1)
-    title(['All tissues - Abs. diff. in ', char(outputNames(selOut))])
-else
-    title(['Tissue ', num2str(nTissue), ' - Abs. diff. in '...
-        char(outputNames(selOut))])
-end
-grid on
-xlabel('Dose threshold(Gy)')
-ylabel('Difference')
-
-%%
-% This block plots the mean and std of the abs. difference of the selected 
-% output for simulations X_0_X and X_1_1 as a function of the studied
-% Dthres values
-
-diffMeanTArrest = [];
-diffStdTArrest = [];
-for i = 1:length(tTArrest)
-    diffMeanTArrest = [diffMeanTArrest...
-        mean(outputAbsDiff(par(:, colTArrest) == tTArrest(i)))];
-    diffStdTArrest = [diffStdTArrest...
-        std(outputAbsDiff(par(:, colTArrest) == tTArrest(i)))];
-end
-
-nfig = nfig + 1;
-figure(nfig)
-errorbar(tTArrest, diffMeanTArrest, diffStdTArrest,...
-    '-s', 'MarkerSize', 10, 'MarkerEdgeColor', 'k', 'MarkerFaceColor', 'k')
-if(nTissue == -1)
-    title(['All tissues - Abs. diff. in ', char(outputNames(selOut))])
-else
-    title(['Tissue ', num2str(nTissue), ' - Abs. diff. in '...
-        char(outputNames(selOut))])
-end
-grid on
-xlabel('TArrest (h)')
-ylabel('Difference')
-
-%%
-% This block plots the mean and std of the selected output for simulations
-% X_0_X and X_1_1 as a function of the studied dose values
-
-withoutMeanDose = [];
-withoutStdDose = [];
-withMeanDose = [];
-withStdDose = [];
-for i = 1:length(tDose)
-    withoutMeanDose = [withoutMeanDose...
-        mean(output(par(:, colDose) == tDose(i), 1))];
-    withoutStdDose = [withoutStdDose...
-        mean(output(par(:, colDose) == tDose(i), 3))];
-    withMeanDose = [withMeanDose...
-        mean(output(par(:, colDose) == tDose(i), 2))];
-    withStdDose = [withStdDose...
-        mean(output(par(:, colDose) == tDose(i), 4))];
-end
-
-nfig = nfig + 1;
-figure(nfig)
-hold on
-errorbar(tDose, withoutMeanDose, withoutStdDose,...
-    'sb', 'MarkerSize', 10, 'MarkerEdgeColor', 'b', 'MarkerFaceColor', 'b')
-errorbar(tDose, withMeanDose, withStdDose,...
-    'sr', 'MarkerSize', 10, 'MarkerEdgeColor', 'r', 'MarkerFaceColor', 'r')
-hold off
-if(nTissue == -1)
-    title(['All tissues - ', char(outputNames(selOut))])
-else
-    title(['Tissue ', num2str(nTissue), ' - '...
-        char(outputNames(selOut))])
-end
 legend(withoutN, withN, 'location', 'northeast')
 grid on
-ylim([0, inf])
-xlabel('Dose (Gy)')
+xlabel(parName)
 ylabel(outputNames(selOut))
 
 %%
-% This block plots the mean and std of the abs. difference of the selected 
+% This block plots the mean and std of the abs. difference of the selected
 % output for simulations X_0_X and X_1_1 as a function of the studied
-% dose values
+% values of the parameter par
 
-diffMeanDose = [];
-diffStdDose = [];
-for i = 1:length(tDose)
-    diffMeanDose = [diffMeanDose...
-        mean(outputAbsDiff(par(:, colDose) == tDose(i)))];
-    diffStdDose = [diffStdDose...
-        std(outputAbsDiff(par(:, colDose) == tDose(i)))];
+diffMean = zeros(1, length(parT));
+diffStd = zeros(1, length(parT));
+
+for i = 1:length(parT)
+    diffMean(i) = mean(outputAbsDiff(par(:, parCol) == parT(i)));
+    diffStd(i) = std(outputAbsDiff(par(:, parCol) == parT(i)));
 end
 
 nfig = nfig + 1;
 figure(nfig)
-errorbar(tDose, diffMeanDose, diffStdDose,...
-    's', 'MarkerSize', 10, 'MarkerEdgeColor', 'k', 'MarkerFaceColor', 'k')
+errorbar(parT, diffMean, diffStd, '-s', 'MarkerSize', 10,...
+    'MarkerEdgeColor', 'k', 'MarkerFaceColor', 'k')
 if(nTissue == -1)
     title(['All tissues - Abs. diff. in ', char(outputNames(selOut))])
 else
@@ -380,27 +241,67 @@ else
         char(outputNames(selOut))])
 end
 grid on
-ylim([0, inf])
-xlabel('Dose (Gy)')
+xlabel(parName)
 ylabel('Difference')
 
 %%
-% This block plots the mean of the abs. difference of the selected output
-% for simulations X_0_X and X_1_1 as a function of the studied
-% dose and TTum values
+selPar1 = 4;
+switch selPar1
+    case 1
+        parName1 = 'TTum (h)';
+        parT1 = tTum;
+        parCol1 = colTTum;
+    case 2
+        parName1 = 'Dose threshold (Gy)';
+        parT1 = tDose;
+        parCol1 = colDose;
+    case 3
+        parName1 = 'TArrest (h)';
+        parT1 = tTArrest;
+        parCol1 = colTArrest;
+    case 4
+        parName1 = 'Dose (Gy)';
+        parT1 = tDose;
+        parCol1 = colDose;        
+end
 
-diffTTumDose = zeros(length(tTTum), length(tDose));
-for i = 1:length(tTTum)
-    for j = 1:length(tDose)
-        diffTTumDose(i, j) =...
-            mean(outputAbsDiff(par(:, colTTum) == tTTum(i) &...
-            par(:, colDose) == tDose(j)));
+selPar2 = 4;
+switch selPar2
+    case 1
+        parName2 = 'TTum (h)';
+        parT2 = tTum;
+        parCol2 = colTTum;
+    case 2
+        parName2 = 'Dose threshold (Gy)';
+        parT2 = tDose;
+        parCol2 = colDose;
+    case 3
+        parName2 = 'TArrest (h)';
+        parT2 = tTArrest;
+        parCol2 = colTArrest;
+    case 4
+        parName2 = 'Dose (Gy)';
+        parT2 = tDose;
+        parCol2 = colDose;        
+end
+
+%%
+% This block plots the mean of the abs. difference of the selected output
+% for simulations X_0_X and X_1_1 as a function of the studied values of
+% parameters par1 and par2
+
+diff = zeros(length(parT1), length(parT2));
+for i = 1:length(parT1)
+    for j = 1:length(parT2)
+        diff(i, j) =...
+            mean(outputAbsDiff(par(:, parCol1) == parT1(i) &...
+            par(:, parCol2) == parT2(j)));
     end
 end
 
 nfig = nfig + 1;
 figure(nfig)
-image(diffTTumDose, 'CDataMapping','scaled')
+image(diff, 'CDataMapping','scaled')
 colorbar
 if(nTissue == -1)
     title(['All tissues - Abs. diff. in ', char(outputNames(selOut))])
@@ -408,77 +309,15 @@ else
     title(['Tissue ', num2str(nTissue), ' - Abs. diff. in '...
         char(outputNames(selOut))])
 end
-xlabel('Dose (Gy)')
-ylabel('TTum (h)')
-xticks(1:length(tDose))
-xticklabels(tDose)
-yticks(1:length(tTTum))
-yticklabels(tTTum)
+xlabel(parName2)
+ylabel(parName1)
+xticks(1:length(parT2))
+xticklabels(parT2)
+yticks(1:length(parT1))
+yticklabels(parT1)
 
 %%
-% This block plots the mean of the abs. difference of the selected output
-% for simulations X_0_X and X_1_1 as a function of the studied
-% dose and Dthres values
-
-diffDThresDose = zeros(length(tDThres), length(tDose));
-for i = 1:length(tDThres)
-    for j = 1:length(tDose)
-        diffDThresDose(i, j) =...
-            mean(outputAbsDiff(par(:, colDThres) == tDThres(i) &...
-            par(:, colDose) == tDose(j)));
-    end
-end
-
-nfig = nfig + 1;
-figure(nfig)
-image(diffDThresDose, 'CDataMapping','scaled')
-colorbar
-if(nTissue == -1)
-    title(['All tissues - Abs. diff. in ', char(outputNames(selOut))])
-else
-    title(['Tissue ', num2str(nTissue), ' - Abs. diff. in '...
-        char(outputNames(selOut))])
-end
-xlabel('Dose (Gy)')
-ylabel('Dthres (Gy)')
-xticks(1:length(tDose))
-xticklabels(tDose)
-yticks(1:length(tDThres))
-yticklabels(tDThres)
-
-%%
-% This block plots the mean of the abs. difference of the selected output
-% for simulations X_0_X and X_1_1 as a function of the studied
-% DThres and TTum values
-
-diffTTumDThres = zeros(length(tTTum), length(tDThres));
-for i = 1:length(tTTum)
-    for j = 1:length(tDThres)
-        diffTTumDThres(i, j) =...
-            mean(outputAbsDiff(par(:, colTTum) == tTTum(i) &...
-            par(:, colDThres) == tDThres(j)));
-    end
-end
-
-nfig = nfig + 1;
-figure(nfig)
-image(diffTTumDThres, 'CDataMapping','scaled')
-colorbar
-if(nTissue == -1)
-    title(['All tissues - Abs. diff. in ', char(outputNames(selOut))])
-else
-    title(['Tissue ', num2str(nTissue), ' - Abs. diff. in '...
-        char(outputNames(selOut))])
-end
-xlabel('DThres (Gy)')
-ylabel('TTum (h)')
-xticks(1:length(tDThres))
-xticklabels(tDThres)
-yticks(1:length(tTTum))
-yticklabels(tTTum)
-
-%%
-% This block plots the histogram of the values of the selected 
+% This block plots the histogram of the values of the selected
 % output for simulations X_0_X and X_1_1 and the histogram of the
 % differences between them.
 
@@ -547,7 +386,7 @@ else
 end
 
 %%
-% This blocks plots for all the values of dose, the mean and std of the
+% This block plots for, all the values of dose, the mean and std of the
 % selected output for simulations X_0_X and X_1_1 as a function of the
 % tissue
 
@@ -569,12 +408,12 @@ for i = 1:length(tDose)
     nfig = nfig + 1;
     figure(nfig)
     hold on
-    errorbar(1:nTissues, withoutMeanDose(i, :), withoutStdDose(i, :),...
-        'sb', 'MarkerSize', 10, 'MarkerEdgeColor', 'b',...
-        'MarkerFaceColor', 'b')
-    errorbar(1:nTissues, withMeanDose(i, :), withStdDose(i, :),...
-        'sr', 'MarkerSize', 10, 'MarkerEdgeColor', 'r',...
-        'MarkerFaceColor', 'r')
+    errorbar(1:nTissues, withoutMeanDose(i, tissuesVascDens),...
+        withoutStdDose(i, tissuesVascDens), 'sb', 'MarkerSize', 10,...
+        'MarkerEdgeColor', 'b', 'MarkerFaceColor', 'b')
+    errorbar(1:nTissues, withMeanDose(i, tissuesVascDens),...
+        withStdDose(i, tissuesVascDens), 'sr', 'MarkerSize', 10,...
+        'MarkerEdgeColor', 'r', 'MarkerFaceColor', 'r')
     hold off
     title(['All tissues - ', char(outputNames(selOut)), ' - '...
         num2str(tDose(i)), ' Gy'])
@@ -582,6 +421,32 @@ for i = 1:length(tDose)
     grid on
     ylim([0, inf])
     xticks(1:nTissues)
+    xticklabels(num2cell(tissuesVascDens))
+    xlabel('Tissue')
+    ylabel(outputNames(selOut))
+end
+
+%%
+% This block plots, for all the values of dose, the mean and std of the
+% abs. differences of the selected output for simulations X_0_X and X_1_1
+% as a function of the tissue
+
+absDiffDose = abs(output(:, 1, :) - output(:, 2, :));
+absDiffDose = permute(absDiffDose, [1, 3, 2]);
+
+for i = 1:length(tDose)
+    nfig = nfig + 1;
+    figure(nfig)
+    hold on
+    plot(1:nTissues, absDiffDose(i, tissuesVascDens), 'sk', 'MarkerSize',...
+        10, 'MarkerFaceColor', 'k')
+    title(['All tissues - Abs. diff. in ', char(outputNames(selOut))...
+        ' - ', num2str(tDose(i)), ' Gy'])
+    legend(withoutN, withN, 'location', 'northeast')
+    grid on
+    ylim([0, inf])
+    xticks(1:nTissues)
+    xticklabels(num2cell(tissuesVascDens))
     xlabel('Tissue')
     ylabel(outputNames(selOut))
 end
