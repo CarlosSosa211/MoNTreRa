f31791b87a91f01bd6af0f51b521444c117776f0
CarlosSosa211 <avefenix.211@gmail.com>
2019-04-18 17:28:34 +0200

    #include guards added

--- src/alloc.hpp
+++ src/alloc.hpp
@@ -1,4 +1,9 @@
+#ifndef DEF_ALLOC
+#define DEF_ALLOC
+
 double **alloc2D(const int nRow, const int nCol);
 double ***alloc3D(const int nLayer, const int nRow, const int nCol);
 void free2D(double **X, const int nRow);
 void free3D(double ***X, const int nLayer, const int nRow);
+
+#endif
