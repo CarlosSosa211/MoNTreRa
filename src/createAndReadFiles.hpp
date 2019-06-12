e3e50acff865283d6ffd0cc4f73438f4774c8a8d
CarlosSosa211 <avefenix.211@gmail.com>
2019-06-12 17:46:36 +0200

    inPO2 is no longer needed

--- src/createAndReadFiles.hpp
+++ src/createAndReadFiles.hpp
@@ -19,14 +19,12 @@ int createInFiles(const int nrow, const int ncol, const int nlayer,
                   const double vascDens, const double sigmaVasc,
                   std::vector<bool> &inVes);
 void readInFiles(const std::string nFInTissueDim, const std::string nFInTum,
-                 const std::string nFInVes, const std::string nFInPO2,
-                 int &nrow, int &ncol, int &nlayer, double &cellSize,
-                 std::vector<bool> &inTum, std::vector<bool> &inVes,
-                 std::vector<double> &inPO2);
+                 const std::string nFInVes, int &nrow, int &ncol, int &nlayer,
+                 double &cellSize, std::vector<bool> &inTum,
+                 std::vector<bool> &inVes);
 void readInFiles(const std::string nFInTissueDim, const std::string nFInVes,
-                 const std::string nFInPO2, int &nrow, int &ncol, int &nlayer,
-                 double &cellSize, std::vector<bool> &inVes,
-                 std::vector<double> &inPO2);
+                 int &nrow, int &ncol, int &nlayer, double &cellSize,
+                 std::vector<bool> &inVes);
 void readInFilesOxy(const std::string nFInTissueOxy, bool &art, int &nrow,
                     int &ncol, int &nlayer, double &cellSize, double &vascDens,
                     double &sigmaVasc);
