142a8a91b7c4101217973bac027ef7971b3b37f2
CarlosSosa211 <avefenix.211@gmail.com>
2020-01-29 17:48:43 +0100

    Parameters are now selected at the initial block

--- Matlab/diffScalOutputsArt.m
+++ Matlab/diffScalOutputsArt.m
@@ -11,11 +11,110 @@ close all
 % - ouputNames: the names of the outputs.
 
 nfig = 0;
-path = '../../Carlos/Results/Diff/Art_5Rep/';
-par = load([path, '/comb.res']);
+
+% path = '../../Carlos/Results/Diff/ArtDens_5Rep/';
+% par = load([path, '/combDens.res']);
+
+% par = par * 100;
+% tTumDens = unique(par(:, 1));
+% tVascDens = unique(par(:, 2));
+% parName = {'d_{tum}', 'd_{vasc}'};
+% parUnit = {' (%)', ' (%)'};
+% parUnitTitle = {'%', '%'};
+% parT = {tTumDens, tVascDens};
+% 
+% selPar1 = 1;
+% selPar2 = 2;
+% parName1 = char(parName(selPar1));
+% parName2 = char(parName(selPar2));
+% parUnit1 = char(parUnit(selPar1));
+% parUnit2 = char(parUnit(selPar2));
+% parT1 = cell2mat(parT(selPar1));
+% parT2 = cell2mat(parT(selPar2));
+
+
+% dtum = 0.9 and dvasc = 0.11 is in fact dtum = 0.89 and dvasc = 0.11
+% path = '../../Carlos/Results/Diff/Art_5Rep/';
+% par = load([path, '/combDens.res']);
+
+% par(:, 1) = par(:, 1) * 100;
+% par(:, 3) = par(:, 3) * 100;
+% tTumDens = unique(par(:, 1));
+% tSigmaTum = unique(par(:, 2));
+% tVascDens = unique(par(:, 3));
+% tSigmaVasc = unique(par(:, 4));
+% parName = {'d_{tum}', '\sigma_{tum}', 'd_{vasc}', '\sigma_{vasc}'};
+% parUnit = {' (%)', '', ' (%)', ''};
+% parUnitTitle = {'%', '', '%', ''};
+% parT = {tTumDens, tSigmaTum, tVascDens, tSigmaVasc};
+
+% selPar1 = 3;
+% selPar2 = 4;
+% parName1 = char(parName(selPar1));
+% parName2 = char(parName(selPar2));
+% parUnit1 = char(parUnit(selPar1));
+% parUnit2 = char(parUnit(selPar2));
+% parT1 = cell2mat(parT(selPar1));
+% parT2 = cell2mat(parT(selPar2));
+
+% selPar1 = 1;
+% selPar2 = 2;
+% selPar3 = 3;
+% selPar4 = 4;
+% parName1 = char(parName(selPar1));
+% parName2 = char(parName(selPar2));
+% parName3 = char(parName(selPar3));
+% parName4 = char(parName(selPar4));
+% parUnitTitle1 = char(parUnitTitle(selPar1));
+% parUnitTitle2 = char(parUnitTitle(selPar2));
+% parUnitTitle3 = char(parUnitTitle(selPar3));
+% parUnitTitle4 = char(parUnitTitle(selPar4));
+% parUnit1 = char(parUnit(selPar1));
+% parUnit2 = char(parUnit(selPar2));
+% parUnit3 = char(parUnit(selPar3));
+% parUnit4 = char(parUnit(selPar4));
+% parT1 = cell2mat(parT(selPar1));
+% parT2 = cell2mat(parT(selPar2));
+% parT3 = cell2mat(parT(selPar3));
+% parT4 = cell2mat(parT(selPar4));
+
+path = '../../Carlos/Results/Diff/ArtDensSigmaTum_5Rep/';
+par = load([path, '/combDens.res']);
+
 par(:, 1) = par(:, 1) * 100;
 par(:, 3) = par(:, 3) * 100;
+tTumDens = unique(par(:, 1));
+tSigmaTum = unique(par(:, 2));
+tVascDens = unique(par(:, 3));
+parName = {'d_{tum}', '\sigma_{tum}', 'd_{vasc}'};
+parUnit = {' (%)', '', ' (%)'};
+parUnitTitle = {'%', '', '%'};
+parT = {tTumDens, tSigmaTum, tVascDens};
 
+% selPar1 = 3;
+% selPar2 = 4;
+% parName1 = char(parName(selPar1));
+% parName2 = char(parName(selPar2));
+% parUnit1 = char(parUnit(selPar1));
+% parUnit2 = char(parUnit(selPar2));
+% parT1 = cell2mat(parT(selPar1));
+% parT2 = cell2mat(parT(selPar2));
+
+selPar1 = 1;
+selPar2 = 2;
+selPar3 = 3;
+parName1 = char(parName(selPar1));
+parName2 = char(parName(selPar2));
+parName3 = char(parName(selPar3));
+parUnitTitle1 = char(parUnitTitle(selPar1));
+parUnitTitle2 = char(parUnitTitle(selPar2));
+parUnitTitle3 = char(parUnitTitle(selPar3));
+parUnit1 = char(parUnit(selPar1));
+parUnit2 = char(parUnit(selPar2));
+parUnit3 = char(parUnit(selPar3));
+parT1 = cell2mat(parT(selPar1));
+parT2 = cell2mat(parT(selPar2));
+parT3 = cell2mat(parT(selPar3));
 nOut = 15;
 
 fileNames = {'/endTreatTumDens.res', '/3MonTumDens.res'...
@@ -34,15 +133,10 @@ outputNames = {'Tumour density at the end of treat. (%)'...
     'Time to kill 99% of tumour cells (h)', 'Recurrence probability'...
     'Tumour density at recurrence (%)', 'Recurrence time (h)'};
 
-tTumDens = unique(par(:, 1));
-tSigmaTum = unique(par(:, 2));
-tVascDens = unique(par(:, 3));
-tSigmaVasc = unique(par(:, 4));
-
 %%
-% This block asks the user to select the output to be
-% studied. Then, it reads the corresponding files.
-% The following intermediate variables are considered:
+% This block asks the user to select the output to be studied. Then,
+% it reads the corresponding files. The following intermediate variables
+% are considered:
 % - output: a matrix containing the values of the scalar output(s) in
 % question. For the single output case, (1 <= selOut <= nOut), each row
 % corresponds to a combination of parameters. The first column corresponds
@@ -67,19 +161,6 @@ elseif(selOut == -1)
 end
 
 %%
-parName = {'d_{tum}', '\sigma_{tum}', 'd_{vasc}', '\sigma_{vasc}'};
-parUnit = {' (%)', '', ' (%)', ''};
-parT = {tTumDens, tSigmaTum, tVascDens, tSigmaVasc};
-selPar1 = 3;
-selPar2 = 4;
-parName1 = char(parName(selPar1));
-parName2 = char(parName(selPar2));
-parUnit1 = char(parUnit(selPar1));
-parUnit2 = char(parUnit(selPar2));
-parT1 = cell2mat(parT(selPar1));
-parT2 = cell2mat(parT(selPar2));
-
-%%
 % This block plots the the selected output as a function of the studied
 % values of parameters par1 and par2
 output12 = zeros(length(parT1), length(parT2));
@@ -91,9 +172,19 @@ for i = 1:length(parT1)
     end
 end
 
+% clims = [min(min(output12)), max(max(output12))];
+% minColOutput12 = min(output12);
+% minOutput12 = min(minColOutput12);
+% maxColOutput12 = max(output12);
+% maxOutput12 = max(maxColOutput12);
+% clims = [min(minColOutput12(minColOutput12 > minOutput12))...
+%     max(maxColOutput12(maxColOutput12 < maxOutput12))];
+clims = [0, 1];
+
 nfig = nfig + 1;
 figure(nfig)
-image(output12, 'CDataMapping','scaled')
+% imagesc(output12)
+imagesc(output12, clims)
 colorbar
 title(char(outputNames(selOut)), 'fontsize', 22)
 ax = gca;
@@ -107,37 +198,44 @@ yticks(1:length(parT1))
 yticklabels(parT1)
 
 %%
-parName = {'d_{tum}', '\sigma_{tum}', 'd_{vasc}', '\sigma_{vasc}'};
-parUnitTitle = {'%', '', '%', ''};
-parUnit = {' (%)', '', ' (%)', ''};
-parT = {tTumDens, tSigmaTum, tVascDens, tSigmaVasc};
-selPar1 = 1;
-selPar2 = 2;
-selPar3 = 3;
-selPar4 = 4;
-parName1 = char(parName(selPar1));
-parName2 = char(parName(selPar2));
-parName3 = char(parName(selPar3));
-parName4 = char(parName(selPar4));
-parUnitTitle1 = char(parUnitTitle(selPar1));
-parUnitTitle2 = char(parUnitTitle(selPar2));
-parUnitTitle3 = char(parUnitTitle(selPar3));
-parUnitTitle4 = char(parUnitTitle(selPar4));
-parUnit1 = char(parUnit(selPar1));
-parUnit2 = char(parUnit(selPar2));
-parUnit3 = char(parUnit(selPar3));
-parUnit4 = char(parUnit(selPar4));
-parT1 = cell2mat(parT(selPar1));
-parT2 = cell2mat(parT(selPar2));
-parT3 = cell2mat(parT(selPar3));
-parT4 = cell2mat(parT(selPar4));
+% This block plots the the selected output as a function of the studied
+% values of parameters par1, par2 and par3
+% clims = [min(output(:, 1)), max(output(:, 1))];
+clims = [0, 1];
+output12 = zeros(length(parT1), length(parT2));
+for k = 1:length(parT3)
+        for i = 1:length(parT1)
+            for j = 1:length(parT2)
+                output12(i, j) =...
+                    output(par(:, selPar1) == parT1(i) &...
+                    par(:, selPar2) == parT2(j) &... 
+                    par(:, selPar3) == parT3(k), 1);
+            end
+        end
+        
+        nfig = nfig + 1;
+        figure(nfig)
+        imagesc(output12, clims)
+        colorbar
+        title([char(outputNames(selOut)), ' (', char(parName3), ' = '... 
+            num2str(parT3(k)), parUnitTitle3, ')'] , 'fontsize', 22)
+        ax = gca;
+        ax.FontSize = 22;
+        
+        xlabel([parName2, parUnit2], 'fontsize', 22)
+        ylabel([parName1, parUnit1], 'fontsize', 22)
+        xticks(1:length(parT2))
+        xticklabels(parT2)
+        yticks(1:length(parT1))
+        yticklabels(parT1)
+end
 
 %%
 % This block plots the the selected output as a function of the studied
 % values of parameters par1, par2, par3 and par4
-% clims = [min(output(:, 1)), max(output(:, 1))];
-clims = [0, 5e4];
-% output12 = zeros(length(parT1), length(parT2));
+clims = [min(output(:, 1)), max(output(:, 1))];
+% clims = [0, 1];
+output12 = zeros(length(parT1), length(parT2));
 for k = 1:length(parT3)
     for l = 1:length(parT4)
         for i = 1:length(parT1)
